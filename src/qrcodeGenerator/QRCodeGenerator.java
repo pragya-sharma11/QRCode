5d7d58f32e02f004e4ef19367a0492a65a35592c
github <pragya.sharma2_cs19@gla.ac.in>
2021-06-10 20:07:53 +0530

    add path , where we want to store our qrcode photo.

--- src/qrcodeGenerator/QRCodeGenerator.java
+++ src/qrcodeGenerator/QRCodeGenerator.java
@@ -3,5 +3,6 @@ package qrcodeGenerator;
 public class QRCodeGenerator {
     public static void main(String[] args)throws Exception {
         String data = "https://www.glauniversity.com";
+        String path = "E:\\QRCode.jpg";
     }
 }
