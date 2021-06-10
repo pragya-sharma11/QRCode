8526a67d4fd1a6a086eaa6fe6ba05026ba51b8a4
github <pragya.sharma2_cs19@gla.ac.in>
2021-06-10 20:05:59 +0530

    create a variable to store the data of qr code.

--- src/qrcodeGenerator/QRCodeGenerator.java
+++ src/qrcodeGenerator/QRCodeGenerator.java
@@ -1,5 +1,7 @@
 package qrcodeGenerator;
 
 public class QRCodeGenerator {
-
+    public static void main(String[] args)throws Exception {
+        String data = "https://www.glauniversity.com";
+    }
 }
