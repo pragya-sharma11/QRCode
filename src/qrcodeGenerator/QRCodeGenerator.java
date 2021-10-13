6743ad8d778ad7de40994a685e643b43a7280f0d
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-13 22:34:50 +0530

    commit

--- src/qrcodeGenerator/QRCodeGenerator.java
+++ src/qrcodeGenerator/QRCodeGenerator.java
@@ -1,8 +1,21 @@
 package qrcodeGenerator;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+
+import java.io.File;
+
 public class QRCodeGenerator {
     public static void main(String[] args)throws Exception {
-        String data = "https://www.glauniversity.com";
-        String path = "E:\\QRCode.jpg";
+        String data = "https://www.google.com";
+
+
+        String path = "C:\\Users\\pragya\\IdeaProjects\\QRCode";
+        BitMatrix matrix = new MultiFormatWriter().encode(data, BarcodeFormat.QR_CODE, 80, 500 );
+        MatrixToImageWriter.writeToFile(matrix, "JPG", new File(path+".jpg"));
+
+        System.out.println("Finish Process!!");
     }
 }
